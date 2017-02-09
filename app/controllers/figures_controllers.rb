dbe7ad98bbd793da9f28596f0621d72193d4a751
github username <github email address>
2017-02-09 16:04:14 -0500

    first commit

---
+++ app/controllers/figures_controllers.rb
@@ -0,0 +1,13 @@
+class FiguresController < ApplicationController
+
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
+  get '/figures/new' do
+    erb :'/figures/new'
+  end
+
+end
